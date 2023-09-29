d5e1664576f3aafd6673c24e03ae8244cd926a25
sunnatillo91 <sunnatillo19912006@gmail.com>
2023-09-29 21:30:42 +0500

    inheritance updated

--- index.php
+++ index.php
@@ -1,79 +1,18 @@
 <?php
 
-// Inheritance
-// extends - the key word to create inheritance from class 
-// parent - child 
-// overriding - parent clasda mavjud clasni child clasda boshqa ko'rinishda yozish
-// final - bu so'z yordamida klassdan yoki metoddan boshqa meros olib bo'lmasligini belgilash mumkin
-
-class Car
+class Production
 {
-    // property, attribute
+    public $product;
+    public $operator;
 
-    public $model;
-    public $year;
-
-    public function __construct($model, $year)
+    public function laboratory()
     {
-        $this->model = $model;
-        $this->year = $year;
-    }
-    //methods
-    public function drive()
-    {
-        return 'driving';
-    }
-
-    public function getFuel()
-    {
-        return 'fueling';
-    }
-
-    public function setModel($model)
-    {
-        $this->model = $model;
+        return "Laboratory";
     }
 }
 
-class ElectricCar extends Car {
-    public $battery;
+$wire = new Production();
+// $wire->laboratory();
 
-    public function charge(){
-        return "charging";
-    }
-}
-
-final class RaceCar extends Car {   // bu classdan meros olib bo'lmaydi final so'zi borligi uchun
-
-}
-
-class PublicCar extends Car {
-
-}
-
-$tesla = new ElectricCar('tesla', '2023');
-$tesla->battery = '45000 mA';
-$tesla->charge();
-
-var_dump($tesla->charge());
-
-
-
-
-
-// $audi = new Car('Audi', '2020');
- 
-// $audi->setModel('Audi-prime');
-// echo $audi->model;
-
-// $bmw = new Car();
-// $merc = new Car();
-
-// $audi->model = 'Audi';
-// $audi->year = '2022';
-
-// echo $audi->drive() . ' ';
-// echo $audi->model . ' ' . $audi->year;
-// // var_dump($bmw);
-
-// var_dump($audi->drive());
+// echo $wire->laboratory();
+var_dump($wire->laboratory());
---
+++ indexes.php
@@ -0,0 +1,79 @@
+<?php
+
+// Inheritance
+// extends - the key word to create inheritance from class 
+// parent - child 
+// overriding - parent clasda mavjud clasni child clasda boshqa ko'rinishda yozish
+// final - bu so'z yordamida klassdan yoki metoddan boshqa meros olib bo'lmasligini belgilash mumkin
+
+class Car
+{
+    // property, attribute
+
+    public $model;
+    public $year;
+
+    public function __construct($model, $year)
+    {
+        $this->model = $model;
+        $this->year = $year;
+    }
+    //methods
+    public function drive()
+    {
+        return 'driving';
+    }
+
+    public function getFuel()
+    {
+        return 'fueling';
+    }
+
+    public function setModel($model)
+    {
+        $this->model = $model;
+    }
+}
+
+class ElectricCar extends Car {
+    public $battery;
+
+    public function charge(){
+        return "charging";
+    }
+}
+
+final class RaceCar extends Car {   // bu classdan meros olib bo'lmaydi final so'zi borligi uchun
+
+}
+
+class PublicCar extends Car {
+
+}
+
+$tesla = new ElectricCar('tesla', '2023');
+$tesla->battery = '45000 mA';
+$tesla->charge();
+
+var_dump($tesla->charge());
+
+
+
+
+
+// $audi = new Car('Audi', '2020');
+ 
+// $audi->setModel('Audi-prime');
+// echo $audi->model;
+
+// $bmw = new Car();
+// $merc = new Car();
+
+// $audi->model = 'Audi';
+// $audi->year = '2022';
+
+// echo $audi->drive() . ' ';
+// echo $audi->model . ' ' . $audi->year;
+// // var_dump($bmw);
+
+// var_dump($audi->drive());
